9f61b5a7e8b93f7e7e1a34cef59611479e5e58bc
Dekel <dekel@lusha.co>
2018-07-21 14:41:09 +0300

    Added $joinRelation tests

--- test/index.test.js
+++ test/index.test.js
@@ -266,7 +266,30 @@ describe('Feathers Objection Service', () => {
       })
     })
 
-    it('allows filtering by relation field with joinEager quieres', () => {
+    it('allows filtering by relation field with joinEager queries', () => {
+      return employees
+        .find({
+          query: {
+            $joinEager: 'company',
+            'company.name': {
+              $like: 'google'
+            }
+          }
+        })
+        .then(data => {
+          expect(data.length).to.equal(1)
+          expect(data[0].company.name).to.equal('Google')
+        })
+    })
+
+    it('allows joinRelation queries', () => {
+      return employees.find({ query: { $joinRelation: 'company', $eager: 'company' } }).then(data => {
+        expect(data[0].company).to.be.ok
+        expect(data[1].company).to.be.ok
+      })
+    })
+
+    it('allows filtering by relation field with joinRelation queries', () => {
       return employees
         .find({
           query: {
