f0e093deb5a2cfbad6d5853d9cc7662e7a4650ae
rob <robbyphillips@gmail.com>
2020-11-20 11:20:18 -0600

    add better error messages

--- src/error-handler.js
+++ src/error-handler.js
@@ -25,46 +25,50 @@ export default function errorHandler (error) {
         feathersError = new errors.BadRequest(message, error.data);
         break;
       case 'RelationExpression':
+        feathersError = new errors.BadRequest('Invalid Relation Expression');
+        break;
       case 'UnallowedRelation':
+        feathersError = new errors.BadRequest('Unallowed Relation Expression');
+        break;
       case 'InvalidGraph':
-        feathersError = new errors.BadRequest(message);
+        feathersError = new errors.BadRequest('Invalid Relation Graph');
         break;
       default:
-        feathersError = new errors.BadRequest(message);
+        feathersError = new errors.BadRequest('Unknown Validation Error');
     }
   } else if (error instanceof NotFoundError) {
     feathersError = new errors.NotFound(message);
   } else if (error instanceof UniqueViolationError) {
-    feathersError = new errors.Conflict(message, {
+    feathersError = new errors.Conflict(`${error.columns.join(', ')} must be unique`, {
       columns: error.columns,
       table: error.table,
       constraint: error.constraint
     });
   } else if (error instanceof NotNullViolationError) {
-    feathersError = new errors.BadRequest(message, {
+    feathersError = new errors.BadRequest(`${error.column.join(', ')} must not be null`, {
       column: error.column,
       table: error.table
     });
   } else if (error instanceof ForeignKeyViolationError) {
-    feathersError = new errors.Conflict(message, {
+    feathersError = new errors.Conflict('Foreign Key Violation', {
       table: error.table,
       constraint: error.constraint
     });
   } else if (error instanceof CheckViolationError) {
-    feathersError = new errors.BadRequest(message, {
+    feathersError = new errors.BadRequest('Check Violation', {
       table: error.table,
       constraint: error.constraint
     });
   } else if (error instanceof ConstraintViolationError) {
-    feathersError = new errors.Conflict(message, {
+    feathersError = new errors.Conflict(`Constraint Violation`, {
       columns: error.columns,
       table: error.table,
       constraint: error.constraint
     });
   } else if (error instanceof DataError) {
-    feathersError = new errors.BadRequest(message);
+    feathersError = new errors.BadRequest('Invalid Data');
   } else if (error instanceof DBError) {
-    feathersError = new errors.GeneralError(message);
+    feathersError = new errors.GeneralError('Unknown Database Error');
   } else {
     feathersError = new errors.GeneralError(message);
   }
